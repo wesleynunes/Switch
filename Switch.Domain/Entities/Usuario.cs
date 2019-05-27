da5e02d66abba1cf9514d6f1aa716ee6dd8c5a1d
wesley_nunes <wesleysilva.ti@gmail.com>
2019-05-27 19:18:33 -0300

    Atualizando

---
+++ Switch.Domain/Entities/Usuario.cs
@@ -0,0 +1,25 @@
+﻿using Switch.Domain.Entities.Enums;
+using System;
+
+namespace Switch.Domain.Entities
+{
+    public class Usuario
+    {
+        public int Id { get; private set; }
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Senha { get; set; }
+
+        public DateTime DataNascimento { get; set; }
+
+        public SexoEnum Sexo { get; set; }
+
+        public string UrlFoto { get; set; }        
+
+    }
+}
